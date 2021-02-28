57ba02387da08150f57239c3e31244ee0e686cbb
nrowley <nelsonononivu2002@gmail.com>
2021-02-28 13:13:48 +0000

    remove nextjs install tailwind

--- client/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-
-export default function Home() {
-  return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  );
-}
---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-purple-500">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
