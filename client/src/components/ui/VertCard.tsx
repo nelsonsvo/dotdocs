c5a653ecb5ff4c1b4f8bc7818a55bf0ed511cbe7
nrowley <nelsonononivu2002@gmail.com>
2021-03-20 14:59:59 +0000

    make modal for settings page

--- client/src/components/ui/VertCard.tsx
+++ client/src/components/ui/VertCard.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 interface Props {
+  children?: React.ReactNode;
   src?: string;
-  children?: JSX.Element;
 }
 
-export const VertCard = (props: Props) => {
+export const VertCard = ({ src, children }: Props) => {
   return (
     <>
-      <div className="bg-blue w-full p-10  flex justify-center font-sans">
-        <div className="rounded bg-white w-80 p-5 ">{props.children}</div>
+      <div className="bg-blue w-full p-10 flex justify-center font-sans cursor-pointer">
+        <div className="rounded bg-white w-80 p-5 ">{children}</div>
       </div>
     </>
   );
--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MainBody from "../components/MainBody";
+import Modal from "../components/ui/Modal";
+import { VertCard } from "../components/ui/VertCard";
 
 interface Props {}
 
 const Settings = ({}: Props) => {
-  return <div></div>;
+  const [folderModal, setFoldersModal] = useState(false);
+
+  return (
+    <MainBody>
+      <Modal open={folderModal} />
+      <div>
+        <div className="grid grid-cols-3 m-auto mt-5 text-gray-800">
+          <div onClick={() => setFoldersModal(true)}>
+            <VertCard>
+              <div className="flex justify-center ">
+                <svg className="w-6 h-6 mr-5 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                </svg>
+                <h3 className="text-xl font-semibold tracking-wide">Folders</h3>
+              </div>
+            </VertCard>
+          </div>
+          <VertCard>
+            <div className="flex justify-center">
+              <svg className="w-6 h-6 mr-5 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+              </svg>
+              <h3 className="text-xl font-semibold tracking-wide text-center justify-center">Users & Groups</h3>
+            </div>
+          </VertCard>
+          <VertCard>
+            <h3 className="text-xl font-semibold tracking-wide">...</h3>
+          </VertCard>
+        </div>
+      </div>
+    </MainBody>
+  );
 };
 
 export default Settings;
