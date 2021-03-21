29a22b2f3a544eee31f661f9b55b351efe7aaebc
nrowley <nelsonononivu2002@gmail.com>
2021-03-21 19:18:17 +0000

    add 4 columns to settings page add configurable card height

--- client/src/components/ui/VertCard.tsx
+++ client/src/components/ui/VertCard.tsx
@@ -3,13 +3,14 @@ import React from "react";
 interface Props {
   children?: React.ReactNode;
   src?: string;
+  height?: string;
 }
 
-export const VertCard = ({ src, children }: Props) => {
+export const VertCard = ({ src, children, height }: Props) => {
   return (
     <>
       <div className="bg-blue w-full p-10 flex justify-center font-sans cursor-pointer">
-        <div className="rounded bg-white w-80 p-5 ">{children}</div>
+        <div className={`rounded bg-white w-80 p-5 h-${height}`}>{children}</div>
       </div>
     </>
   );
--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -10,7 +10,7 @@ const Settings: React.FC<SettingsProps> = () => {
     <>
       <MainBody>
         <div>
-          <div className="grid grid-cols-3 m-auto mt-5 text-gray-800">
+          <div className="grid grid-cols-4 m-auto mt-5 text-gray-800">
             <Link to="/settings/folders">
               <VertCard>
                 <div className="flex justify-center ">
@@ -22,12 +22,15 @@ const Settings: React.FC<SettingsProps> = () => {
               </VertCard>
             </Link>
             <VertCard>
-              <div className="flex justify-center">
+              <div className="flex  justify-center">
                 <svg className="w-6 h-6 mr-5 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                 </svg>
                 <h3 className="text-xl font-semibold tracking-wide text-center justify-center">Users & Groups</h3>
               </div>
+            </VertCard>
+            <VertCard>
+              <h3 className="text-xl font-semibold tracking-wide">...</h3>
             </VertCard>
             <VertCard>
               <h3 className="text-xl font-semibold tracking-wide">...</h3>
