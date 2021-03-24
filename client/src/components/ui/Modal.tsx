3e2cca1428769e8ca6093e31dd141b2a374e011d
nrowley <nelsonononivu2002@gmail.com>
2021-03-24 22:01:09 +0000

    fix modal

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -27,8 +27,8 @@ const Modal = ({ children, open, setModalState, title }: Props) => {
             {children}
 
             <div className="flex justify-end pt-2">
-              <button className="px-4 bg-transparent cursor-pointer p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
-              <button onClick={() => setModalState(false)} className="modal-close cursor-pointer px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">
+              <button className="px-4 bg-transparent cursor-pointer p-3 rounded-lg text-blue-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
+              <button onClick={() => setModalState(false)} className="modal-close cursor-pointer px-4 bg-blue-500 p-3 rounded-lg text-white hover:bg-indigo-400">
                 Close
               </button>
             </div>
