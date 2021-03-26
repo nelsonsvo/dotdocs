c2453c8db7dccae638215d0d37689c72583d24bf
nrowley <nelsonononivu2002@gmail.com>
2021-03-26 21:35:37 +0000

    add styling and layout to applications

--- client/src/components/settings/Applications.tsx
+++ client/src/components/settings/Applications.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Settings from "../../pages/Settings";
+import ApplicationCard from "../cards/ApplicationCard";
+import Input from "../form/Input";
 import SettingSectionCard from "../ui/SettingSectionCard";
 
 interface ApplicationsProps {}
@@ -12,11 +14,22 @@ const Applications: React.FC<ApplicationsProps> = () => {
         <div className="mt-10 sm:mt-0">
           <div className="px-4 py-5 bg-white sm:p-6">
             <div className="grid grid-cols-6 gap-6 text-left">
+              <div className="col-span-6">
+                <h3 className="text-gray-700 tracking-wide text-md md:text-xl">Existing Applications</h3>
+              </div>
+              <div className="col-span-6">
+                <div className="flex flex-col">
+                  <ApplicationCard name="Accounts Payable" onEdit={() => console.log("editing")} onDelete={() => console.log("deleting")} />
+                  <ApplicationCard name="Invoices" mt={3} onEdit={() => console.log("editing")} onDelete={() => console.log("deleting")} />
+                  <ApplicationCard name="Emails" mt={3} onEdit={() => console.log("editing")} onDelete={() => console.log("deleting")} />
+                </div>
+              </div>
+
               <div className="col-span-6 sm:col-span-3">
-                <label htmlFor="first_name" className="block text-sm font-medium text-gray-700">
-                  First name
+                <label htmlFor="last_name" className="block  text-sm font-medium text-gray-700">
+                  Last name
                 </label>
-                <input type="text" name="first_name" id="first_name" className="mt-1 focus:ring-blue-500 focus:border-blue-500 w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                <input type="text" name="last_name" id="last_name" className="mt-1 focus:ring-blue-500 focus:border-blue-500 w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
               </div>
 
               <div className="col-span-6 sm:col-span-3">
@@ -74,7 +87,10 @@ const Applications: React.FC<ApplicationsProps> = () => {
             </div>
           </div>
           <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            <button
+              type="submit"
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
               Save
             </button>
           </div>
