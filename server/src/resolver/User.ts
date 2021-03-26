2068ddb5e546305748fc743ed02d96f449e6a278
nrowley <nelsonononivu2002@gmail.com>
2021-03-26 21:39:51 +0000

    add the application

--- server/src/resolver/User.ts
+++ server/src/resolver/User.ts
@@ -1,13 +1,18 @@
 import { User } from "./../entity/User";
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import * as argon2 from "argon2";
+
+type LoginError = {
+  username: string;
+  password: string;
+};
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
   async users() {
     return User.find();
   }
-  @Mutation(() => User)
+  @Mutation(() => User, { nullable: true })
   async login(@Arg("username") username: string, @Arg("password") password: string) {
     const user = await User.findOne({ username });
     if (user) {
