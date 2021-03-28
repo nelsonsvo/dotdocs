0cf8f0fd9c5de6bd4ae58059d317428ed1aa08b2
nrowley <nelsonononivu2002@gmail.com>
2021-03-28 22:13:37 +0100

    run migration

---
+++ server/src/migration/1616956726775-test.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class test1616956726775 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
