1af9c0fdecb90100a8bc48efb6f5cc037bf2cc08
nrowley <nelsonononivu2002@gmail.com>
2021-03-29 18:21:24 +0100

    add a get retrieval query

--- client/src/graphql/queries/Application.ts
+++ client/src/graphql/queries/Application.ts
@@ -1,10 +1,24 @@
 import { gql } from "@apollo/client";
 
 export const GET_APPLICATIONS = gql`
-  {
+  query {
     applications {
       id
       name
+    }
+  }
+`;
+
+export const GET_RETRIEVAL_TEMPLATES = gql`
+  query {
+    applications {
+      id
+      name
+      updatedAt
+      fields {
+        id
+        name
+      }
     }
   }
 `;
