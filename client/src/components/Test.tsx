855c7e24e8222aa64e4c595d09ca1ad34a4757a8
nrowley <nelsonononivu2002@gmail.com>
2021-03-29 18:19:58 +0100

    create a test component

---
+++ client/src/components/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RetrievalBody from "./layouts/RetrievalBody";
+
+interface Props {}
+
+export const Test = (props: Props) => {
+  return (
+    <RetrievalBody>
+      <h1>hello world</h1>
+    </RetrievalBody>
+  );
+};
