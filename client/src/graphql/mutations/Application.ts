fd258d08078e8dc60d38febd23e2f38d331a7655
nrowley <nelsonononivu2002@gmail.com>
2021-03-29 18:21:14 +0100

    add a application deletion mutation

--- client/src/graphql/mutations/Application.ts
+++ client/src/graphql/mutations/Application.ts
@@ -1,10 +1,16 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_APPLICATION = gql`
-  mutation($name: String!, $fields: [AppField!]!) {
+  mutation CreateApplication($name: String!, $fields: [appField!]!) {
     createApplication(name: $name, fields: $fields) {
       id
       name
     }
+  }
+`;
+
+export const DELETE_APPLICATION = gql`
+  mutation DeleteApplication($id: String!) {
+    deleteApplication(id: $id)
   }
 `;
