30c763e595c96e9a237bdc3d2fcea80aaf41b5a1
nrowley <nelsonononivu2002@gmail.com>
2021-03-30 18:50:29 +0100

    add indexing page

---
+++ client/src/pages/Index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import IndexBody from "../components/layouts/IndexBody";
+
+interface IndexProps {}
+
+const Index: React.FC<IndexProps> = () => {
+  return (
+    <IndexBody>
+      <h1>indexing place</h1>
+    </IndexBody>
+  );
+};
+
+export default Index;
