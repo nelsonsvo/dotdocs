3a11c180b2cb27cfb1a5f95fd2fc56f0a6d741c0
nrowley <nelsonononivu2002@gmail.com>
2021-03-30 18:49:56 +0100

    create upload link for file upload

--- client/src/graphql/ApolloClient.ts
+++ client/src/graphql/ApolloClient.ts
@@ -1,6 +1,11 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+
+const URI = "http://localhost:4000/graphql";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
+  link: createUploadLink({
+    uri: URI,
+  }),
 });
