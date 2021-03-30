929a6b9b8a95376048ed179ff85d97f5427cb22c
nrowley <nelsonononivu2002@gmail.com>
2021-03-30 18:50:43 +0100

    add retrieval page

---
+++ client/src/pages/Retrieval.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RetrievalBody from "../components/layouts/RetrievalBody";
+
+interface Props {}
+
+export const Retrieval = (props: Props) => {
+  return (
+    <RetrievalBody>
+      <h1>hello world</h1>
+    </RetrievalBody>
+  );
+};
