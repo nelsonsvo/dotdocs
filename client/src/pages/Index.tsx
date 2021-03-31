7cbb54d6f8e9d1969543d7d321c674fd4d189404
nrowley <nelsonononivu2002@gmail.com>
2021-03-31 20:14:29 +0100

    add index provider

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,13 +1,20 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
 import IndexBody from "../components/layouts/IndexBody";
+import { IndexContext } from "../context/IndexContext";
+import { GET_RETRIEVAL_TEMPLATES } from "../graphql/queries/Application";
 
 interface IndexProps {}
 
 const Index: React.FC<IndexProps> = () => {
+  const { loading, error, data } = useQuery(GET_RETRIEVAL_TEMPLATES);
+
   return (
-    <IndexBody>
-      <h1>indexing place</h1>
-    </IndexBody>
+    <IndexContext.Provider value={data}>
+      <IndexBody>
+        <h1>indexing place</h1>
+      </IndexBody>
+    </IndexContext.Provider>
   );
 };
 
