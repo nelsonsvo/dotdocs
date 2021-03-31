d62b6b82bfdf046c44521c98fcde6d9f4fc089e1
nrowley <nelsonononivu2002@gmail.com>
2021-03-31 20:14:40 +0100

    add retrieval provider

--- client/src/pages/Retrieval.tsx
+++ client/src/pages/Retrieval.tsx
@@ -1,12 +1,21 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
 import RetrievalBody from "../components/layouts/RetrievalBody";
+import { RetrievalContext } from "../context/RetrievalContext";
+import { GET_RETRIEVAL_TEMPLATES } from "../graphql/queries/Application";
 
-interface Props {}
+interface RetrievalProps {}
 
-export const Retrieval = (props: Props) => {
+const Retrieval: React.FC<RetrievalProps> = () => {
+  const { loading, error, data, refetch } = useQuery(GET_RETRIEVAL_TEMPLATES);
+
   return (
-    <RetrievalBody>
-      <h1>hello world</h1>
-    </RetrievalBody>
+    <RetrievalContext.Provider value={data}>
+      <RetrievalBody>
+        <h1>hello world</h1>
+      </RetrievalBody>
+    </RetrievalContext.Provider>
   );
 };
+
+export default Retrieval;
