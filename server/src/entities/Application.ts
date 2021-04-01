d777d46a53822c19092a88f586cfe9e017962ef6
nrowley <nelsonononivu2002@gmail.com>
2021-04-02 00:28:08 +0100

    make application an input object

--- server/src/entities/Application.ts
+++ server/src/entities/Application.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType } from "type-graphql";
+import { Field, InputType, ObjectType } from "type-graphql";
 import {
   BaseEntity,
   BeforeInsert,
@@ -14,6 +14,7 @@ import { AppField } from "./AppField";
 import { AppFile } from "./AppFile";
 
 @ObjectType("Application")
+@InputType("application")
 @Entity()
 export class Application extends BaseEntity {
   @Field(() => String)
