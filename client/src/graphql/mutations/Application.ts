ae3e837c1bf4dc0fc172d51f3829845ea800d358
nrowley <nelsonononivu2002@gmail.com>
2021-04-02 00:25:29 +0100

    add mutation for indexing a file

--- client/src/graphql/mutations/Application.ts
+++ client/src/graphql/mutations/Application.ts
@@ -17,6 +17,17 @@ export const DELETE_APPLICATION = gql`
 
 export const SINGLE_FILE_UPLOAD = gql`
   mutation SingleUpload($file: Upload!, $id: String!) {
-    singleUpload(file: $file, id: $id)
+    singleUpload(file: $file, id: $id) {
+      id
+      filename
+      mimetype
+      location
+    }
+  }
+`;
+
+export const INDEX_FILE = gql`
+  mutation($id: String!, $fields: [AppFieldInput!]!) {
+    indexFile(id: $id, fields: $fields)
   }
 `;
