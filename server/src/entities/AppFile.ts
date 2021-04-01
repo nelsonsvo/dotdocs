36a5725108d833acc0553b0000a0141abe523643
nrowley <nelsonononivu2002@gmail.com>
2021-04-01 19:55:30 +0100

    add description

--- server/src/entities/AppFile.ts
+++ server/src/entities/AppFile.ts
@@ -6,11 +6,17 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryColumn,
   UpdateDateColumn,
 } from "typeorm";
 import { v4 } from "uuid";
 import { Application } from "./Application";
+import { FileField } from "./FileField";
+
+/*
+This entity stores the Files in the application
+*/
 
 @ObjectType("AppFile")
 @Entity()
@@ -34,6 +40,10 @@ export class AppFile extends BaseEntity {
   @Field((type) => Application)
   @ManyToOne(() => Application, (application) => application.files)
   application: Application;
+
+  @Field((type) => [FileField])
+  @OneToMany(() => FileField, (field) => field.file)
+  fields: FileField[];
 
   @Field(() => Date)
   @CreateDateColumn()
