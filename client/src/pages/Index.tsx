ec9e970f06aab944d38b70a9af981ecf6da3efc3
nrowley <nelsonononivu2002@gmail.com>
2021-04-02 00:25:46 +0100

    add iframe to preview uploaded files

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,19 +1,34 @@
 import { useQuery } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
+import Iframe from "react-iframe";
 import IndexBody from "../components/layouts/IndexBody";
 import { IndexContext } from "../context/IndexContext";
+import { IndexFileContext, UploadedFile } from "../context/IndexFileContext";
 import { GET_RETRIEVAL_TEMPLATES } from "../graphql/queries/Application";
 
 interface IndexProps {}
 
 const Index: React.FC<IndexProps> = () => {
+  const FILE_SERVER_URL = process.env.REACT_APP_FILE_SERVER_URL;
   const { loading, error, data } = useQuery(GET_RETRIEVAL_TEMPLATES);
+  const [uploadedFiles, setUploadedFiles] = useState<UploadedFile[]>([]);
 
   return (
     <IndexContext.Provider value={data}>
-      <IndexBody>
-        <h1>indexing place</h1>
-      </IndexBody>
+      <IndexFileContext.Provider value={{ uploadedFiles, setUploadedFiles }}>
+        <IndexBody>
+          {uploadedFiles.length > 0 ? (
+            <Iframe
+              url={FILE_SERVER_URL + uploadedFiles[0].location}
+              id="myId"
+              className="w-full h-full"
+              position="relative"
+            />
+          ) : (
+            <div className="mt-5 text-2xl">There are no files left to index..</div>
+          )}
+        </IndexBody>
+      </IndexFileContext.Provider>
     </IndexContext.Provider>
   );
 };
