57dea34a0cd148a7bd830b2c7194c5cd24f088ab
nrowley <nelsonononivu2002@gmail.com>
2021-04-02 18:36:59 +0100

    add watermark to indexing page

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -26,11 +26,28 @@ const Index: React.FC<IndexProps> = () => {
             <Iframe
               url={FILE_SERVER_URL + uploadedFiles[0].location}
               id="myId"
-              className="w-full h-full"
-              position="relative"
+              className="w-full h-full "
+              position="absolute"
             />
           ) : (
-            <div className="mt-5 text-2xl">There are no files left to index..</div>
+            <div className="flex flex-row justify-center h-screen">
+              <div className="mt-5 text-2xl text-gray-600 opacity-10 flex flex-col justify-center h-screen">
+                <svg
+                  className="w-80 h-80"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="0.75"
+                    d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                  />
+                </svg>
+              </div>
+            </div>
           )}
         </IndexBody>
       </IndexFileContext.Provider>
