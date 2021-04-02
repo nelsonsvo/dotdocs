f27d0006dec29aa5ec7b609f7d62cf6d104c0509
nrowley <nelsonononivu2002@gmail.com>
2021-04-02 18:37:07 +0100

    add watermark to retrieval

--- client/src/pages/Retrieval.tsx
+++ client/src/pages/Retrieval.tsx
@@ -10,9 +10,26 @@ const Retrieval: React.FC<RetrievalProps> = () => {
   const { loading, error, data, refetch } = useQuery(GET_RETRIEVAL_TEMPLATES);
 
   return (
-    <RetrievalContext.Provider value={data}>
+    <RetrievalContext.Provider value={{ data, error }}>
       <RetrievalBody>
-        <h1>hello world</h1>
+        <div className="flex flex-row justify-center h-screen">
+          <div className="mt-5 text-2xl text-gray-600 opacity-10 flex flex-col justify-center h-screen">
+            <svg
+              className="w-80 h-80"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="0.75"
+                d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z"
+              />
+            </svg>
+          </div>
+        </div>
       </RetrievalBody>
     </RetrievalContext.Provider>
   );
