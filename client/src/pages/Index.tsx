5f95e10bb0025b7f168f7ec5173139baed9fa2a0
nrowley <nelsonononivu2002@gmail.com>
2021-04-02 15:37:48 +0100

    memoize context

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Iframe from "react-iframe";
 import IndexBody from "../components/layouts/IndexBody";
 import { IndexContext } from "../context/IndexContext";
@@ -13,9 +13,14 @@ const Index: React.FC<IndexProps> = () => {
   const { loading, error, data } = useQuery(GET_RETRIEVAL_TEMPLATES);
   const [uploadedFiles, setUploadedFiles] = useState<UploadedFile[]>([]);
 
+  const indexFileValue = useMemo(() => ({ uploadedFiles, setUploadedFiles }), [
+    uploadedFiles,
+    setUploadedFiles,
+  ]);
+
   return (
     <IndexContext.Provider value={{ data, error }}>
-      <IndexFileContext.Provider value={{ uploadedFiles, setUploadedFiles }}>
+      <IndexFileContext.Provider value={indexFileValue}>
         <IndexBody>
           {uploadedFiles.length > 0 ? (
             <Iframe
