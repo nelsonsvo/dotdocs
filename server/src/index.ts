ddc67771223f81d1d06b84dc141f5a9934ef571c
nrowley <nelsonononivu2002@gmail.com>
2021-04-03 21:50:01 +0100

    add redis and express session

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,71 @@
+import { ApolloServer } from "apollo-server-express";
+import connectRedis from "connect-redis";
 import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import session from "express-session";
 import { graphqlUploadExpress } from "graphql-upload";
 import { join } from "path";
+import redis from "redis";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import { ApplicationResolver } from "./resolvers/Application";
 import { FileResolver } from "./resolvers/File";
 import { UserResolver } from "./resolvers/User";
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
+import { MyContext } from "./types/ContextType";
+
+dotenv.config();
+declare module "express-session" {
+  interface Session {
+    userId: string;
+    userType: string;
+  }
+}
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
 
 const main = async () => {
-  const schema = await buildSchema({
-    resolvers: [UserResolver, ApplicationResolver, FileResolver],
-  });
-  const apolloServer = new ApolloServer({ schema, uploads: false });
-
   const app = express();
-  app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
+
+  console.log(process.env.SECRET!);
+
   app.use(
     cors({
-      origin: "*",
+      origin: process.env.DOT_DOCS_CLIENT_URL,
       credentials: true,
     })
   );
+  app.use(
+    session({
+      name: "uid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 30, // 30mins
+        httpOnly: true, //accessible in frontend client?
+        secure: false, //only works in https?
+        sameSite: "lax", //protect against csrf
+      },
+      saveUninitialized: false,
+
+      secret: process.env.SECRET!,
+      resave: false,
+    })
+  );
+
+  const schema = await buildSchema({
+    resolvers: [UserResolver, ApplicationResolver, FileResolver],
+  });
+
+  const apolloServer = new ApolloServer({
+    schema,
+    uploads: false,
+    context: ({ req, res }): MyContext => ({ req, res }),
+  });
+
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
+
   app.use("/files", express.static(join(__dirname, "../files")));
   apolloServer.applyMiddleware({ app, cors: false });
 
