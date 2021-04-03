caf88b4a7cb9fcc8af9e10c8a72c454a01509830
nrowley <nelsonononivu2002@gmail.com>
2021-04-03 19:44:30 +0100

    add auth state and wrap protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { ApolloProvider } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthContext, IAuth } from "./context/AuthContext";
 import { client } from "./graphql/ApolloClient";
 import Dashboard from "./pages/Dashboard";
 import Index from "./pages/Index";
@@ -10,16 +12,25 @@ import Retrieval from "./pages/Retrieval";
 import Settings from "./pages/Settings";
 
 function App() {
+  const [auth, setAuth] = useState<IAuth>({
+    loggedIn: false,
+    userType: null,
+  });
+
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
         <div className="App">
           <Switch>
-            <Route exact path="/" component={Login} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/retrieval" component={Retrieval} />
-            <Route path="/index" component={Index} />
+            <AuthContext.Provider value={{ auth, setAuth }}>
+              <Route exact path="/" component={Login} />
+              <ProtectedRoute>
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/settings" component={Settings} />
+                <Route path="/retrieval" component={Retrieval} />
+                <Route path="/index" component={Index} />
+              </ProtectedRoute>
+            </AuthContext.Provider>
           </Switch>
         </div>
       </BrowserRouter>
