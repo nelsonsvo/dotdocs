f6e431b0b49751ec4f02846994b0829366cc5642
nrowley <nelsonononivu2002@gmail.com>
2021-04-21 23:05:35 +0100

    test

--- client/src/pages/Retrieval.tsx
+++ client/src/pages/Retrieval.tsx
@@ -117,7 +117,7 @@ const Retrieval: React.FC<RetrievalProps> = () => {
               <svg
                 className="w-80 h-80"
                 fill="none"
-                stroke="currentColor"
+                stroke="currentColors"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
