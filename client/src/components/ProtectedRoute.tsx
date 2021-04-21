561e03295ff9d8eeb08c0e293449c917cee9488e
nrowley <nelsonononivu2002@gmail.com>
2021-04-21 23:01:16 +0100

    add auth checker

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,26 +1,32 @@
-import React, { useContext } from "react";
+import { useLazyQuery } from "@apollo/client";
+import React, { useContext, useEffect } from "react";
 import { Redirect } from "react-router";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext, IAuthContext } from "../context/AuthContext";
+import { ME } from "../graphql/queries/Login";
 
 //this component is a wrapper component to pass protected routes through
 interface ProtectedRouteProps {}
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { auth, setAuth } = useContext(AuthContext);
-  // const [checkCookie] = useLazyQuery(ME, {
-  //   fetchPolicy: "network-only",
-  //   onError: () => setAuth({ ...auth, loggedIn: false }),
-  // });
+  const { auth, setUserAuth } = useContext<IAuthContext>(AuthContext);
+  const [checkCookie] = useLazyQuery(ME, {
+    fetchPolicy: "network-only",
+    onError: () => setUserAuth(false),
+  });
 
-  // //check if cookie has expired every minute
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     checkCookie();
-  //   }, 60000);
+  //check if cookie has expired every minute
+  useEffect(() => {
+    const interval = setInterval(() => {
+      checkCookie();
+    }, 60000);
 
-  //   return () => clearInterval(interval);
-  // }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-  return auth.loggedIn ? <>{children}</> : <Redirect to="/" />;
+  return auth.loggedIn || Boolean(localStorage.getItem("isAuth")) ? (
+    <>{children}</>
+  ) : (
+    <Redirect to="/" />
+  );
 };
 export default ProtectedRoute;
