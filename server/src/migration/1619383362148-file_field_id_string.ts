4b0db81621d6d1b70e04826bd3c73cd567bcba6b
nrowley <nelsonononivu2002@gmail.com>
2021-04-25 23:20:55 +0100

    run migration

---
+++ server/src/migration/1619383362148-file_field_id_string.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class fileFieldIdString1619383362148 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
