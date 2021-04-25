0ae88d777f8deef6c1e3ab0baba8f58f5b4498cd
nrowley <nelsonononivu2002@gmail.com>
2021-04-25 23:19:09 +0100

    add searching to the retrieval bar

--- client/src/components/RetrievalBar.tsx
+++ client/src/components/RetrievalBar.tsx
@@ -1,5 +1,6 @@
 import { useLazyQuery } from "@apollo/client";
 import React, { useCallback, useContext, useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
 import { NavLink } from "react-router-dom";
 import { RetrievalContext } from "../context/RetrievalContext";
 import { GET_FILES } from "../graphql/queries/Application";
@@ -13,9 +14,14 @@ const RetrievalBar: React.FC<RetrievalBarProps> = ({ className }) => {
     RetrievalContext
   );
 
+  const { register, handleSubmit, reset } = useForm();
+
   const [getFiles] = useLazyQuery(GET_FILES, {
     fetchPolicy: "network-only",
-    onCompleted: (data) => setSearchResults(data.getFiles),
+    onCompleted: (data) => {
+      console.log(data.getFiles);
+      setSearchResults(data.getFiles);
+    },
   });
 
   const setTemplateState = useCallback(
@@ -31,13 +37,28 @@ const RetrievalBar: React.FC<RetrievalBarProps> = ({ className }) => {
     [setCurrentTemplate, setSearchResults]
   );
 
-  const searchApplication = useCallback(async () => {
-    getFiles({
-      variables: {
-        id: currentTemplate.id,
-      },
-    });
-  }, [currentTemplate.id, getFiles]);
+  const searchApplication = useCallback(
+    async (fieldArr) => {
+      getFiles({
+        variables: {
+          id: currentTemplate.id,
+          fields: fieldArr,
+        },
+      });
+    },
+    [currentTemplate.id, getFiles]
+  );
+
+  const onSubmit = handleSubmit((data) => {
+    let fieldArr = [];
+    for (const [key, value] of Object.entries(data)) {
+      fieldArr.push({
+        name: key,
+        value,
+      });
+    }
+    searchApplication(fieldArr);
+  });
 
   const selectRef = useRef<HTMLSelectElement>(null);
 
@@ -50,83 +71,85 @@ const RetrievalBar: React.FC<RetrievalBarProps> = ({ className }) => {
   return (
     <div className={className}>
       <div className="min-h-screen h-screen w-100 flex-shrink-0 antialiased bg-white text-gray-700 border-r">
-        <div className="flex flex-col bg-white h-screen ">
-          <div className="flex items-center justify-center">
-            <NavLink to="/dashboard">
-              <img className="h-12" src="/images/dotdocs.png" alt="" />
-            </NavLink>
-          </div>
-          <div>
-            <div className="text-gray-800 text-left mt-3 border-t border-b py-1 bg-gray-200 border-gray-200 text-ml mb-5">
-              <h3 className="px-3 tracking-widest text-center">FIND A DOCUMENT</h3>
+        <form onSubmit={onSubmit}>
+          <div className="flex flex-col bg-white h-screen ">
+            <div className="flex items-center justify-center">
+              <NavLink to="/dashboard">
+                <img className="h-12" src="/images/dotdocs.png" alt="" />
+              </NavLink>
             </div>
-            <div className="px-4">
-              <div className="">
-                <select
-                  id="type"
-                  ref={selectRef}
-                  onChange={(e) => setTemplateState(e)}
-                  name="type"
-                  className=" block w-full py-2 px-3 border-t border-gray-200 bg-white  shadow-sm focus:outline-none focus:ring-gray-100 focus:border-gray-100 sm:text-sm"
-                >
-                  {data &&
-                    data.applications.map((template: any) => {
-                      return (
-                        <option key={template.id} id={template.id}>
-                          {template.name}
-                        </option>
-                      );
+            <div>
+              <div className="text-gray-800 text-left mt-3 border-t border-b py-1 bg-gray-200 border-gray-200 text-ml mb-5">
+                <h3 className="px-3 tracking-widest text-center">FIND A DOCUMENT</h3>
+              </div>
+              <div className="px-4">
+                <div className="">
+                  <select
+                    id="type"
+                    ref={selectRef}
+                    onChange={(e) => setTemplateState(e)}
+                    name="type"
+                    className=" block w-full py-2 px-3 border-t border-gray-200 bg-white  shadow-sm focus:outline-none focus:ring-gray-100 focus:border-gray-100 sm:text-sm"
+                  >
+                    {data &&
+                      data.applications.map((template: any) => {
+                        return (
+                          <option key={template.id} id={template.id}>
+                            {template.name}
+                          </option>
+                        );
+                      })}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="overflow-y-auto overflow-x-hidden py-5 px-3 flex-grow text-left">
+              {data && (
+                <div className="flex flex-col gap-5 mt-3">
+                  {data.applications
+                    .filter((template: any) => template.name === currentTemplate.name)
+                    .map((template: any) => {
+                      return template.fields.map((f: any) => {
+                        return (
+                          <div key={f.id} className="px-2">
+                            <label
+                              htmlFor={f.name}
+                              className="block  text-sm font-medium text-gray-700"
+                            >
+                              {f.name}
+                            </label>
+                            <input
+                              type={f.type.toLowerCase()}
+                              name={f.name}
+                              ref={register}
+                              className="mt-1 focus:ring-blue-500 focus:border-blue-500 w-full shadow-sm sm:text-sm border-gray-300 rounded-sm"
+                            />
+                          </div>
+                        );
+                      });
                     })}
-                </select>
+                </div>
+              )}
+            </div>
+            <div className="object-bottom mb-5 border-t">
+              <div className="grid grid-cols-2 px-5 gap-3 mt-5 ">
+                <button
+                  type="submit"
+                  className="w-full justify-center py-2 px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Search
+                </button>
+                <button
+                  onClick={() => console.log("hello world")}
+                  type="button"
+                  className="w-full justify-center py-2 px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-800 bg-gray-300 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Clear
+                </button>
               </div>
             </div>
           </div>
-          <div className="overflow-y-auto overflow-x-hidden py-5 px-3 flex-grow text-left">
-            {data && (
-              <div className="flex flex-col gap-5 mt-3">
-                {data.applications
-                  .filter((template: any) => template.name === currentTemplate.name)
-                  .map((template: any) => {
-                    return template.fields.map((f: any) => {
-                      return (
-                        <div key={f.id} className="px-2">
-                          <label
-                            htmlFor={f.name}
-                            className="block  text-sm font-medium text-gray-700"
-                          >
-                            {f.name}
-                          </label>
-                          <input
-                            type={f.type.toLowerCase()}
-                            name={f.name}
-                            className="mt-1 focus:ring-blue-500 focus:border-blue-500 w-full shadow-sm sm:text-sm border-gray-300 rounded-sm"
-                          />
-                        </div>
-                      );
-                    });
-                  })}
-              </div>
-            )}
-          </div>
-          <div className="object-bottom mb-5 border-t">
-            <div className="grid grid-cols-2 px-5 gap-3 mt-5 ">
-              <button
-                onClick={() => searchApplication()}
-                type="button"
-                className="w-full justify-center py-2 px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Search
-              </button>
-              <button
-                onClick={() => console.log("hello world")}
-                type="button"
-                className="w-full justify-center py-2 px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-800 bg-gray-300 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Clear
-              </button>
-            </div>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
