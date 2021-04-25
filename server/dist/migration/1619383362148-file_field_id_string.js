f41121671891bb2db1db896a8409728ecd4eee69
nrowley <nelsonononivu2002@gmail.com>
2021-04-25 23:20:08 +0100

    run typeorm migration

---
+++ server/dist/migration/1619383362148-file_field_id_string.js
@@ -0,0 +1,22 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+class fileFieldIdString1619383362148 {
+    up(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+        });
+    }
+    down(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+        });
+    }
+}
+exports.fileFieldIdString1619383362148 = fileFieldIdString1619383362148;
+//# sourceMappingURL=1619383362148-file_field_id_string.js.map
