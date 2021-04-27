3bcc01c22de0405cb1b637701ed5b1bfa5410fd3
nrowley <nelsonononivu2002@gmail.com>
2021-04-27 17:41:04 +0100

    add types for iterators

--- client/src/pages/Retrieval.tsx
+++ client/src/pages/Retrieval.tsx
@@ -11,6 +11,26 @@ interface ParamTypes {
   id: string;
 }
 
+interface IField {
+  name: string;
+  value: string;
+  id: string;
+}
+
+interface IApplication {
+  id: string;
+  fields: Array<IField>;
+}
+
+interface IRow {
+  id: string;
+  key: string;
+  name: string;
+  resizable: boolean;
+  sortable: boolean;
+  editor: JSX.Element;
+}
+
 const Retrieval: React.FC<RetrievalProps> = () => {
   const { id } = useParams<ParamTypes>();
 
@@ -29,7 +49,7 @@ const Retrieval: React.FC<RetrievalProps> = () => {
     if (data && id) {
       setCurrentTemplate({
         id: id,
-        name: data.applications.filter((x: any) => x.id === id)[0].name,
+        name: data.applications.filter((x: IApplication) => x.id === id)[0].name,
       });
     }
   }, [id, data]);
@@ -38,7 +58,7 @@ const Retrieval: React.FC<RetrievalProps> = () => {
     console.log(data);
     if (data) {
       const row = data.applications.filter((x: any) => x.id === currentTemplate.id)[0];
-      let cols = row.fields.map((f: any) => {
+      let cols = row.fields.map((f: IField) => {
         return {
           key: f.name,
           name: f.name,
@@ -53,16 +73,15 @@ const Retrieval: React.FC<RetrievalProps> = () => {
       return cols;
     }
   };
-
   const getRows = () => {
     if (searchResults) {
       let rows: any = [];
-      searchResults.forEach((result: any, index: number) => {
+      searchResults.forEach((result: IApplication) => {
         console.log(result);
         let fields: any = {};
-        result.fields.forEach((f: any) => {
+        result.fields.forEach((f: IField) => {
           console.log(f);
-          fields = { ...fields, id: index, [f.name]: f.value };
+          fields = { ...fields, id: result.id, [f.name]: f.value };
         });
         rows = [...rows, fields];
       });
@@ -71,9 +90,31 @@ const Retrieval: React.FC<RetrievalProps> = () => {
       return rows;
     }
   };
-  function rowKeyGetter(row: any) {
+
+  const rowKeyGetter = (row: IRow) => {
     return row.id;
-  }
+  };
+
+  //for viewing a document or multiple documents
+  const viewDocument = () => {
+    if (selectedRows.size === 1) {
+      selectedRows.forEach((value) => {
+        const res = searchResults.find((f: IField) => f.id === value);
+        console.log(res.location);
+
+        window.open(`/viewer/${res.location.replaceAll("/", "_")}`, "_blank");
+      });
+    } else if (selectedRows.size > 1) {
+      selectedRows.forEach((value) => {
+        const res = searchResults.find((f: IField) => f.id === value);
+        console.log(res);
+        console.log("opening", res.location);
+
+        window.open(`/viewer/${res.location.replaceAll("/", "_")}`, "_blank");
+      });
+    }
+  };
+
   return (
     <RetrievalContext.Provider
       value={{ data, error, currentTemplate, setCurrentTemplate, setSearchResults }}
@@ -81,26 +122,51 @@ const Retrieval: React.FC<RetrievalProps> = () => {
       <RetrievalBody>
         {searchResults.length > 0 ? (
           <div className="flex flex-col">
-            <div className="flex flex-row px-3 py-3 bg-gray-50 border-b border-r justify-start gap-2 text-sm font-light text-gray-800">
-              <button className=" justify-center py-2 px-2 border border-transparent  rounded-sm   hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <h1>View</h1>
-              </button>
-              <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <h1>Remarks</h1>
-              </button>
-              <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <h1>Keywords</h1>
-              </button>
-              <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <h1>Download</h1>
-              </button>
-              <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <h1>Remove</h1>
-              </button>
-              <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <h1>Delete</h1>
-              </button>
-            </div>
+            {selectedRows.size > 0 ? (
+              <div className="flex flex-row px-3 py-3 transition duration-500 ease-linear bg-gray-50 border-b border-r justify-start gap-2 text-sm font-light text-gray-800">
+                <button
+                  onClick={() => viewDocument()}
+                  className=" justify-center py-2 px-2 border border-transparent  rounded-sm   bg-gray-200  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  <h1>View</h1>
+                </button>
+                <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm bg-gray-200  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                  <h1>Remarks</h1>
+                </button>
+                <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   bg-gray-200  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                  <h1>Keywords</h1>
+                </button>
+                <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   bg-gray-200  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                  <h1>Download</h1>
+                </button>
+                <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   bg-gray-200  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                  <h1>Remove</h1>
+                </button>
+                <button className=" justify-center py-2 px-2 border border-transparent   rounded-sm   bg-gray-200  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                  <h1>Delete</h1>
+                </button>
+              </div>
+            ) : (
+              <div className="flex flex-row px-3 py-3 transition duration-500 ease-linear bg-gray-50 border-b border-r justify-center gap-2 text-sm font-light text-gray-800 my-auto">
+                <h1 className="text-xl tracking-wider font-light font-antialiased my-auto">
+                  Documents
+                </h1>
+                <svg
+                  className="w-7 h-7 my-auto"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={1}
+                    d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+            )}
             <DataGrid
               className={"rdg-light fill-grid min-h-screen"}
               rowHeight={50}
