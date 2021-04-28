db87a4add9ac2f4407ed7ad4d8dff7c50798c433
nrowley <nelsonononivu2002@gmail.com>
2021-04-28 21:43:47 +0100

    create modal

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -1,41 +1,104 @@
 import React from "react";
 
-interface Props {
+interface ModalProps {
   open: boolean;
-  children?: React.ReactNode;
-  setModalState: Function;
   title: string;
+  success: boolean;
+  btnLabel: string;
+  setModalOpen: (bool: boolean) => void;
 }
 
-const Modal = ({ children, open, setModalState, title }: Props) => {
-  return (
-    <>
-      <div className={`modal opacity-${open ? "100 modal-active" : "0 pointer-events-none"} fixed w-full h-full top-0 left-0 flex items-center justify-center`} style={{ transition: "opacity 0.25s ease" }}>
-        <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+const Modal: React.FC<ModalProps> = ({
+  open,
+  title,
+  success,
+  btnLabel,
 
-        <div className="modal-container bg-white w-screen  md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
-          <div className="modal-content py-4 text-left px-6">
-            <div className="flex justify-between items-center pb-3">
-              <p className="text-2xl font-bold">{title}</p>
-              <div className="modal-close cursor-pointer z-50">
-                <svg className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                  <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+  setModalOpen,
+  children,
+}) => {
+  const onBtnClick = () => {
+    setModalOpen(!open);
+  };
+
+  return open ? (
+    <div
+      className={`fixed z-10 inset-0 overflow-y-auto `}
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+        ></div>
+
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+          &#8203;
+        </span>
+
+        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div
+                className={`mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10`}
+              >
+                <svg
+                  className={`h-6 w-6 text-blue-500`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
                 </svg>
               </div>
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                  {title}
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">{children}</p>
+                </div>
+              </div>
             </div>
-
-            {children}
-
-            <div className="flex justify-end pt-2">
-              <button className="px-4 bg-transparent cursor-pointer p-3 rounded-lg text-blue-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
-              <button onClick={() => setModalState(false)} className="modal-close cursor-pointer px-4 bg-blue-500 p-3 rounded-lg text-white hover:bg-indigo-400">
-                Close
-              </button>
-            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              type="button"
+              onClick={() => onBtnClick()}
+              className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white bg-${
+                success ? "blue" : "gray"
+              }-600 hover:bg-${
+                success ? "blue" : "gray"
+              }-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-${
+                success ? "blue" : "gray"
+              }-500 sm:ml-3 sm:w-auto sm:text-sm`}
+            >
+              {btnLabel}
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setModalOpen(false);
+              }}
+              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-500 shadow-sm px-4 py-2 bg-gray-500 text-base font-medium text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-100 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Cancel
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
+  ) : (
+    <></>
   );
 };
 
