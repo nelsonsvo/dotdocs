67b076c8a7dd3f6638b6aa378292b9123dc0b401
nrowley <nelsonononivu2002@gmail.com>
2021-05-02 12:15:49 +0100

    add more modal styling and fix tsx

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -42,10 +42,12 @@ const Modal: React.FC<ModalProps> = ({
           <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
               <div
-                className={`mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10`}
+                className={`mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-${
+                  success ? "blue" : "red"
+                }-100 sm:mx-0 sm:h-10 sm:w-10`}
               >
                 <svg
-                  className={`h-6 w-6 text-blue-500`}
+                  className={`h-6 w-6 text-${success ? "blue" : "red"}-500`}
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -53,9 +55,9 @@ const Modal: React.FC<ModalProps> = ({
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
                   />
                 </svg>
@@ -74,13 +76,7 @@ const Modal: React.FC<ModalProps> = ({
             <button
               type="button"
               onClick={() => onBtnClick()}
-              className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white bg-${
-                success ? "blue" : "gray"
-              }-600 hover:bg-${
-                success ? "blue" : "gray"
-              }-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-${
-                success ? "blue" : "gray"
-              }-500 sm:ml-3 sm:w-auto sm:text-sm`}
+              className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm`}
             >
               {btnLabel}
             </button>
