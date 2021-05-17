9c88143b05561d853575c7b2f064885d508d3b80
nrowley <nelsonononivu2002@gmail.com>
2021-05-17 20:46:49 +0100

    change remark icon

--- client/src/components/ui/RemarksDialog.tsx
+++ client/src/components/ui/RemarksDialog.tsx
@@ -75,8 +75,8 @@ const RemarksDialog: React.FC<RemarksDialogProps> = ({ isOpen, setModalOpen, fil
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                    strokeWidth={1.5}
+                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
                   />
                 </svg>
               </div>
