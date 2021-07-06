8463d6652c47a4b9ec4e45a8f04936fdcfc3ff3e
Nelson Rowley <nelsonononivu2002@gmail.com>
2021-07-06 20:55:01 +0100

    use localStorage to persist between page reloads

--- client/src/util/AuthCheck.ts
+++ client/src/util/AuthCheck.ts
@@ -1,23 +1,28 @@
-import { IAuth } from "../context/AuthContext";
 import { Pages } from "./Pages";
 
-export function checkRetrievalTemplateAuth(name: string, auth: IAuth | undefined): boolean {
+export function checkRetrievalTemplateAuth(name: string): boolean {
+  const permissions = JSON.parse(localStorage.getItem("permissions")!);
+
   try {
-    if (auth!.permissions!.includes(name.toUpperCase() + "_RETRIEVAL_TRUE")) {
-      return true;
-    }
+    return permissions.includes(name.toUpperCase() + "_RETRIEVAL_TRUE");
+  } catch (err) {
     return false;
+  }
+}
+export function checkIndexTemplateAuth(name: string): boolean {
+  const permissions = JSON.parse(localStorage.getItem("permissions")!);
+  try {
+    return permissions.includes(name.toUpperCase() + "_INDEXING_TRUE");
   } catch (err) {
     return false;
   }
 }
 
-export function isPageAuthorised(page: Pages, auth: IAuth | undefined): boolean {
+export function isPageAuthorised(page: Pages): boolean {
+  const permissions = JSON.parse(localStorage.getItem("permissions")!);
+
   try {
-    if (auth!.permissions!.includes(page + "_TRUE")) {
-      return true;
-    }
-    return false;
+    return permissions.includes(page + "_TRUE");
   } catch (err) {
     return false;
   }
